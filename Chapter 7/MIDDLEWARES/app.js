54a06eaf13ca23b2ab0df914e418190d3d30d419
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-05-27 11:56:58 +0530

    Created Logger Middleware

--- Chapter 7/MIDDLEWARES/app.js
+++ Chapter 7/MIDDLEWARES/app.js
@@ -4,25 +4,33 @@ const port= 8080;
 
 //Middleware->Response send
 
-app.use((req, res, next)=>{
-    // let  { query } = req.query;
-    // console.log( query );   //Show we can access the request and response
-    console.log("Hello I am MIDDLEWARE 1");
-    // res.send("MIDDLEWARE FINISHED");
-    return next();
-    console.log("Function after MIDDLEWARW 1");     //NOT EXECUTE
-})
+// app.use((req, res, next)=>{
+//     // let  { query } = req.query;
+//     // console.log( query );   //Show we can access the request and response
+//     console.log("Hello I am MIDDLEWARE 1");
+//     // res.send("MIDDLEWARE FINISHED");
+//     return next();
+//     console.log("Function after MIDDLEWARW 1");     //NOT EXECUTE
+// })
+
+// app.use((req, res, next)=>{
+//     console.log("Hello I am MIDDLEWARE 2");
+//     next();
+// })
 
 app.use((req, res, next)=>{
-    console.log("Hello I am MIDDLEWARE 2");
-    next();
+    req.time= new Date(Date.now()).toString();
+    console.log(req.method, req.hostname, req.path, req.time);
+    return next();
 })
 
 app.get("/", (req, res)=>{
+    console.log("Getting Request on ROOT page")
     res.send("Getting Request on ROOT page");
 })
 
 app.get("/random", (req, res)=>{
+    console.log("Getting request on RANDOM page");
     res.send("Getting request on RANDOM page");
 })
 
