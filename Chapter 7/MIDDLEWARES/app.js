8e7e988cd1cd5fcf556c68359d7d528d7d946480
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-05-27 10:42:51 +0530

    First Middleware

--- Chapter 7/MIDDLEWARES/app.js
+++ Chapter 7/MIDDLEWARES/app.js
@@ -2,8 +2,19 @@ const express= require("express");
 const app= express();
 const port= 8080;
 
+//Middleware->Response send
+
+app.use((req, res)=>{
+    console.log("Hello I am MIDDLEWARE");
+    res.send("Hello I am MIDDLEWARE");
+})
+
 app.get("/", (req, res)=>{
-    res.send("Getting Request on INDEX Route");
+    res.send("Getting Request on ROOT page");
+})
+
+app.get("/random", (req, res)=>{
+    res.send("Getting request on RANDOM page");
 })
 
 app.listen(port, ()=>{
